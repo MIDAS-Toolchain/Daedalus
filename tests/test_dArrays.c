5a41b75b99d194bab3c8e643a6c81265110ea661
McCoy1701 <jkellum819@gmail.com>
2025-12-25 21:23:29 -0500

    starting array test

--- tests/test_dArrays.c
+++ tests/test_dArrays.c
@@ -2,11 +2,75 @@
 
 #include "Daedalus.h"
 
+static int test_destory( void );
+static int test_resize( void );
+static int test_grow( void );
+static int test_append( void );
+static int test_get( void );
+static int test_pop( void );
+static int test_insert( void );
+static int test_remove( void );
+static int test_clear( void );
+
 int main(void)
 {
     printf("=== dArray Tests ===\n\n");
 
     printf("=== All tests passed! ===\n");
     return 0;
+}
+
+static int test_destory( void )
+{
+  
+  return 0;
+}
+
+static int test_resize( void )
+{
+
+  return 0;
+}
+
+static int test_grow( void )
+{
+
+  return 0;
+}
+
+static int test_append( void )
+{
+
+  return 0;
+}
+
+static int test_get( void )
+{
+
+  return 0;
+}
+
+static int test_pop( void )
+{
+
+  return 0;
+}
+
+static int test_insert( void )
+{
+
+  return 0;
+}
+
+static int test_remove( void )
+{
+
+  return 0;
+}
+
+static int test_clear( void )
+{
+
+  return 0;
 }
 
